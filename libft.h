7209e30f26df0f4e3c4c70e5d356e8197d3fb636
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-04 11:43:40 +0100

    to

---
+++ libft.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/04 11:04:26 by adoussau          #+#    #+#             */
+/*   Updated: 2014/11/04 11:06:18 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		isalpha(int c);
+int		isalnum(int c);
+int		isascii(int c);
+int		isdigit(int c);
+int		islower(int c);
+int		isprint(int c);
+int		isupper(int c);
