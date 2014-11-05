081320a36f210850571812ad3df141aa3c9bea0e
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-05 07:23:23 +0100

    ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/05 07:08:00 by adoussau          #+#    #+#             */
+/*   Updated: 2014/11/05 07:22:54 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*ptrdst;
+	const unsigned char	*ptrsrc;
+	
+	ptrdst = (unsigned char *)dst;
+	ptrsrc =(const unsigned char *)src;
+	while (n--)
+		*ptrdst++ = *ptrsrc++;
+
+	return (dst);
+}
