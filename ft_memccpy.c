89a94edc6e5f6532bc58e0ccb5b8ecf43a2c460d
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-05 07:56:01 +0100

    ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/11/05 07:08:00 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/05 07:55:37 by adoussau         ###   ########.fr       */
+/*   Created: 2014/11/05 07:41:29 by adoussau          #+#    #+#             */
+/*   Updated: 2014/11/05 07:54:44 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	unsigned char		*ptrdst;
 	const unsigned char	*ptrsrc;
+	unsigned char		cara;
 
 	ptrdst = (unsigned char *)dst;
 	ptrsrc = (const unsigned char *)src;
+	cara = (unsigned char)c;
 	while (n--)
-		*ptrdst++ = *ptrsrc++;
-	return (dst);
+		if ((*ptrdst++ = *ptrsrc++) == cara)
+			return (ptrdst);
+	return (NULL);
 }
