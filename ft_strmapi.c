7fbd228b0f49f75017dbabadc267f34d743d1f36
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-06 17:26:53 +0100

    strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,33 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 16:53:32 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/06 17:25:35 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/06 17:26:14 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char	*ft_strmap(const char *s, char (*f)(char))
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
 {
-	char *str;
-	char *ret;
+	char			*str;
+	unsigned int	i;
 
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
 	if (!str)
 		return (NULL);
-	ret = str;
-	while (*s)
-	{
-		*str = (*f)(*s);
-		str++;
-		s++;
-	}
-	*str = 0;
-	return (ret);
+	i = 0;
+	while (s[i])
+		str[i++] = (*f)(i++, *s);
+	str[i] = 0;
+	return (str);
 }
