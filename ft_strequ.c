9af2a18bae65607a5c1459b90e4ac73bc1a4c0e6
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-07 13:12:49 +0100

    ft_strequ.c

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,14 +6,20 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/06 17:28:17 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/06 17:42:33 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/07 13:11:13 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_strequ(const char *s1, const char *s2)
 {
-	while (*s1 && *s2)
-		if (*s1++ != *s2++)
+	if (s1 && s2)
+	{
+		while (*s1 && *s2)
+			if (*s1++ != *s2++)
+				return (0);
+		if (*s1 != *s2)
 			return (0);
-	return (1);
+		return (1);
+	}
+	return (0);
 }
