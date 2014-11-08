17c42ac564e3a8d6a36196664d6a998f715e79e6
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-08 19:24:42 +0100

    lstiter.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/04 11:04:26 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/08 19:17:04 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/08 19:23:22 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ t_list				*ft_lstnew(const void *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 
 int					ft_isalpha(int c);
 int					ft_isalnum(int c);
