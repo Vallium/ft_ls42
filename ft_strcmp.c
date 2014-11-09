932167f88f7f4f5d1f55ec89a5a4945f251e60d1
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-09 18:37:32 +0100

    norm

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 08:20:02 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/09 12:38:25 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/09 18:35:38 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int		ft_strcmp(const char *s1, const char *s2)
 {
 	unsigned char *us1;
 	unsigned char *us2;
+
 	us1 = (unsigned char *)s1;
 	us2 = (unsigned char *)s2;
 	while (*us1 || *us2)
