a6186643af907de65d5911a282ab9454bc47353e
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-10 16:16:37 +0100

    split

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,63 +6,69 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 10:23:10 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/10 14:40:10 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/10 16:15:40 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int		countwords(const char *s, char c)
+static int		wc(const char *s, char c)
 {
 	int		i;
+	int		j;
 
 	i = 0;
-	while (*s++)
-		if (*s == c && *(s + 1) != c && *(s + 1))
-			i++;
-	return (i);
+	j = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			++j;
+			while (s[i] != c)
+			{
+				if (!s[i])
+					return (j);
+				++i;
+			}
+		}
+		++i;
+	}
+	return (j);
 }
 
-static size_t	len(const char *s, char c)
+static int		wl(const char *s, int i, int j, char c)
 {
-	size_t		i;
-
-	i = 0;
-	while (*s && *s != c)
-		i++;
-	return (i);
-}
-
-static void		cpy(char *s1, char *s2, char c)
-{
-	while (*s2 != c)
-		*s1++ = *s2++;
-	*s1 = 0;
-}
-
-static char*	new(const char *s, char c)
-{
-	char	*ret;
-
-	ret = (char *)malloc(sizeof(char) * (len(s, c) + 1));
-	cpy(ret, (char *)s, c);
-	return (ret);
+	while (	s[i] != c && s[i])
+	{
+		++i;
+		++j;
+	}
+	return (j);
 }
 
 char		**ft_strsplit(const char *s, char c)
 {
-	char			**tab;
-	char			**ret;
-	unsigned int	i;
+	char**ret;
+	int		i;
+	int		k;
+	int		w;
 
 	if (!s)
+		return (0);
+	w = wc(s, c);
+	ret = (char **)malloc(sizeof(char *) * w + 1);
+	if (!ret)
 		return (NULL);
-	tab = (char **)malloc(sizeof(char *) * countwords(s, c));
-	if (!tab)
-		return (NULL);
-	ret = tab;
-	while (*s)
-		if (*s == c && *(s + 1) != c && *(s + 1))
-			*tab++ = new(s, c);
-	return (tab);
+	i = 0;
+	k = 0;
+	ret[w] = 0;
+	while (k < w)
+	{
+		while (s[i] == c)
+			++i;
+		ret[k] = ft_strsub(s, i, wl(s, i, 0, c));
+		while (s[i] && s[i] != c)
+			i++;
+		++k;
+	}
+	return (ret);
 }
