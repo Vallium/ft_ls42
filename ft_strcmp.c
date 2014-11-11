7e31f7aa8663b224c5ab769760f3ad8aae9ef5ad
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-11 15:30:21 +0100

    rendu

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,11 +6,9 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/05 08:20:02 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/09 18:35:38 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/11 15:25:17 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
