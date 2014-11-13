383edbd4ba3d84d49beb8daa5bd909bcc086dd68
Antoine DOUSSAUD <antoine@doussaud.org>
2014-11-13 17:27:59 +0100

    correction post push

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,7 +6,7 @@
 /*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/08 19:10:11 by adoussau          #+#    #+#             */
-/*   Updated: 2014/11/08 19:13:36 by adoussau         ###   ########.fr       */
+/*   Updated: 2014/11/13 17:24:17 by adoussau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@ void	ft_lstadd(t_list **alst, t_list *new)
 	{
 		if (!(*alst))
 			*alst = new;
-		new->next = *alst;
-		*alst = new;
+		else
+		{
+			new->next = *alst;
+			*alst = new;
+		}
 	}
 }
