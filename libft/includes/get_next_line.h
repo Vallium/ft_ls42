96f7ddc3a9f1702a5db6ce4c8ab25b658b013408
Vallium <aalliot@student.42.fr>
2014-12-10 10:38:27 +0100

    buffsize

--- libft/includes/get_next_line.h
+++ libft/includes/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: aalliot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 12:44:13 by aalliot           #+#    #+#             */
-/*   Updated: 2014/11/29 13:05:58 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/12/10 10:37:34 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 8
+# define BUFF_SIZE 1000
 
 # include "libft.h"
 # include <unistd.h>
