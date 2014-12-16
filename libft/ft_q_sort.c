ca845429986bb2394308bf2b9fe6efed0ae1ce5b
Spectre <antoine@doussaud.org>
2014-12-16 13:41:10 +0100

    norm

--- libft/ft_q_sort.c
+++ libft/ft_q_sort.c
@@ -6,31 +6,31 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/12/16 12:17:19 by aalliot           #+#    #+#             */
-/*   Updated: 2014/12/16 12:17:22 by aalliot          ###   ########.fr       */
+/*   Updated: 2014/12/16 13:32:07 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
 
-void    ft_q_sort(int *tab, int bg, int ed)
+void			ft_q_sort(int *tab, int bg, int ed)
 {
-  const int   pvt = tab[bg];
-  int         lft;
-  int         rgt;
+	const int	pvt = tab[bg];
+	int			lft;
+	int			rgt;
 
-  lft = bg - 1;
-  rgt = ed + 1;
-  if (bg >= ed)
-    return;
-  while(1)
-  {
-    while(tab[--rgt] > pvt);
-    while(tab[++lft] < pvt);
-    if (lft < rgt)
-      ft_swap_int(tab, lft, rgt);
-    else
-      break;
-  }
-  ft_q_sort(tab, bg, rgt);
-  ft_q_sort(tab, rgt + 1, ed);
+	lft = bg - 1;
+	rgt = ed + 1;
+	if (bg >= ed)
+		return ;
+	while (1)
+	{
+		while (tab[--rgt] > pvt);
+		while (tab[++lft] < pvt);
+		if (lft < rgt)
+			ft_swap_int(tab, lft, rgt);
+		else
+			break ;
+	}
+	ft_q_sort(tab, bg, rgt);
+	ft_q_sort(tab, rgt + 1, ed);
 }
