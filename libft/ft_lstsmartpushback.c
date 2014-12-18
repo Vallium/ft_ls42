982035faaad7f95dc3f07dc5d88fbf7d6e99d35a
Vallium <aalliot@student.42.fr>
2014-12-18 17:48:07 +0100

    name

--- libft/ft_lstsmartpushback.c
+++ libft/ft_lstsmartpushback.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsmartpushback.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adoussau <antoine@doussaud.org>            +#+  +:+       +#+        */
+/*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/16 11:04:25 by adoussau          #+#    #+#             */
 /*   Updated: 2014/11/16 11:07:07 by adoussau         ###   ########.fr       */
