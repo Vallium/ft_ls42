2be4b3da467758a5bb0d50921171d4d52d128981
Vallium <aalliot@student.42.fr>
2014-12-30 20:42:59 +0100

    get_opt OK

--- src/ft_get_opt.c
+++ src/ft_get_opt.c
@@ -11,26 +11,37 @@
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+#include <stdio.h>
 
 int				ft_get_opt(int argc, char **argv, t_opt *opt)
 {
 	static int	i = 0;
 	char		*ret;
 
-	if (!argv[opt->nbarg][i])
+/*	if (argv[opt->nbarg + 1][0] == '-' && argv[opt->nbarg + 1][1] == '-') // si arg == "--"
+	{
+		opt->nbarg += 2;
+		return (0);
+	}*/
+	//        -la --
+	if (!argv[opt->nbarg][i]) // si de lire l'arg
 	{
 		i = 0;
 		opt->nbarg++;
-		if (opt->nbarg == argc)
+		if (opt->nbarg == argc) //si plus d'arg
 			return (-1);
 	}
 	if (!i)
 	{
 		if (argv[opt->nbarg][0] == '-' && argv[opt->nbarg][1] != '-'
-			&& argv[opt->nbarg][1])
+			&& argv[opt->nbarg][1]) // si arg valide
 			i++;
 		else
+		{
+			if (argv[opt->nbarg][0] == '-' && argv[opt->nbarg][1] == '-')
+				opt->nbarg++;
 			return (-1);
+		}
 	}
 	ret = ft_strchr(opt->optstr, argv[opt->nbarg][i++]);
 	if (!ret)
