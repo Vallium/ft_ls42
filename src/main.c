fe4a54140c81f0752ccf1665c5d0fcbb12f0ecae
Spectre <antoine@doussaud.org>
2014-12-31 20:29:55 +0100

    ft_burger

--- src/main.c
+++ src/main.c
@@ -38,24 +38,6 @@ void	to_wedge(const char *str, int n)
 		ft_putchar(*str++);
 }
 
-char	*ft_burger(const char *s1, char c, const char *s2)
-{
-	char *ret;
-	char *tmp;
-
-	ret = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
-	tmp = ret;
-	if (!ret)
-		exit(1);
-	while (*s1)
-		*tmp++ = *s1++;
-	*tmp++ = c;
-	while (*s2)
-		*tmp++ = *s2++;
-	*tmp = 0;
-	return(ret);
-}
-
 void swap(void **p1, void **p2)
 {
 	void	*tmp;
