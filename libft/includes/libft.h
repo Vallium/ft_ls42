596a92d2314146c492d23b79afa858d0186585ed
Spectre <antoine@doussaud.org>
2015-01-02 11:56:41 +0100

    rename new varible (pb c++)

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -27,11 +27,11 @@ typedef struct		s_list
 t_list				*ft_lstnew(const void *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
-void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstadd(t_list **alst, t_list *ne);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-void				ft_lstpushback(t_list **lst, t_list *new);
-void				ft_lstsmartpushback(t_list **list, t_list*new);
+void				ft_lstpushback(t_list **lst, t_list *ne);
+void				ft_lstsmartpushback(t_list **list, t_list *ne);
 void				ft_lstsimpledel(t_list **list);
 void				ft_lstsimpledelone(t_list **list);
 
