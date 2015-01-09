c109adb1fd36e81515698b92d1b33053cb3f750b
Anton ALLIOT <anton.alliot@live.se>
2015-01-09 17:06:53 +0100

    file_r_size_cmp.c

---
+++ src/file_r_size_cmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file_size_cmp.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adoussau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/06 12:11:35 by adoussau          #+#    #+#             */
+/*   Updated: 2015/01/06 12:11:39 by adoussau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_ls.h"
+
+int			file_r_size_cmp(void *ptr1, void *ptr2)
+{
+	t_file *f1;
+	t_file *f2;
+
+	f1 = (t_file *)ptr1;
+	f2 = (t_file *)ptr2;
+	if (f1->stats.st_size > f2->stats.st_size)
+		return (1);
+	else if (f1->stats.st_size < f2->stats.st_size)
+		return (-1);
+	return (0);
+}
